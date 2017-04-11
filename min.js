0be150d5b3d29c2124c0bb92b28e163e76709d85
trollocool <trollocool@gmail.com>
2017-04-11 14:22:50 +0200

    Add files via upload

---
+++ min.js
@@ -0,0 +1,10 @@
+/*JS*/
+$('#scroll__top').click(()=>{
+	$('html').animate({scrollTop : 0},600);
+});
+
+$(document).ready(()=>{
+    if($('html').scrollTop()==0){
+        $('html').animate({scrollTop : $('#nav__container').outerHeight()-10},200);
+    }
+});
